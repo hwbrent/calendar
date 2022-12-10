37572de92e05a40cd682d9114c14207e39c30f9c
mustcat <hwbrent@icloud.com>
2022-12-10 22:20:52 +0000

    Swap ViewRadios with ViewButtons

--- src/Calendar/Calendar.jsx
+++ src/Calendar/Calendar.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, createContext, useContext} from 'react';
+import React, { useState, useEffect, createContext} from 'react';
 
 import Day from './Day/Day';
 import Week from './Week/Week';
 import Month from './Month/Month';
+import Year from './Year/Year';
 
 export const DateContext = createContext();
 export const TimetableContext = createContext();
@@ -21,31 +22,17 @@ const Views = {
 };
 
 /**
- * Provides a set of three radio buttons that allows the user to choose
- * how the 
- * @param {object} props
- * @param {Function} props.onChange
- * @returns {JSX.Element}
+ * 
+ * @param {React.props} props
+ * @param {Function} props.onClick
  */
-function ViewRadios(props) {
-    // TODO: allow user to specify default view. Perhaps save value in localStorage?
-    let { onChange } = props;
+function ViewButtons(props) {
     return (
         <div>
-            <label>
-                Day
-                <input type='radio' name="view" value="DAY" onChange={onChange}/>
-            </label>
-
-            <label>
-                Week
-                <input type='radio' name="view" value="WEEK" onChange={onChange} defaultChecked/>
-            </label>
-
-            <label>
-                Month
-                <input type='radio' name="view" value="MONTH" onChange={onChange}/>
-            </label>
+            <button type='button' value="DAY" onClick={props.onClick}>Day</button>
+            <button type='button' value="WEEK" onClick={props.onClick}>Week</button>
+            <button type='button' value="MONTH" onClick={props.onClick}>Month</button>
+            <button type='button' value="YEAR" onClick={props.onClick}>Year</button>
         </div>
     );
 }
@@ -53,7 +40,7 @@ function ViewRadios(props) {
 export default function Calendar(props) {
 
     // The type of calendar view that's being shown.
-    // Default is Week. Can only ever be Day, Week or Month.
+    // Default is Week. Can only ever be Day, Week, Month or Year.
     const [ view, setView ] = useState(Views.WEEK);
 
     /**
@@ -65,17 +52,17 @@ export default function Calendar(props) {
     const [ date, setDate ] = useState(new Date());
 
     /**
-     * Sets {@link view} to the component corresponding to the radio button that was clicked.
-     * @param {'DAY'|'WEEK'|'MONTH'} value - The `value` attribute from the `<input>` tag in {@link ViewRadios}
+     * Sets {@link view} to the component corresponding to the button in {@link ViewButtons} that was clicked.
+     * @param {'DAY'|'WEEK'|'MONTH'|'YEAR'} value - The `value` attribute from the `<input>` tag in {@link ViewButtons}
      */
-    const viewRadiosHandleChange = ({ target: { value } }) => {
+    const viewButtonsHandleChange = ({ target: { value } }) => {
         const viewComponent = Views[value];
         setView(viewComponent);
     };
 
     return (
         <>
-        <ViewRadios onChange={viewRadiosHandleChange}/>
+        <ViewButtons onClick={viewButtonsHandleChange}/>
 
         
         <TimetableContext.Provider value={null}>
@@ -86,60 +73,4 @@ export default function Calendar(props) {
 
         </>
     );
-
 }
-
-/*
-function TimetableVisualiser(props) {
-
-    const dateUseState = useState(new Date());
-    const timetableUseState = useState(sample_timetable);
-
-    const [ timetable, setTimetable ] = timetableUseState;
-
-    const [ viewType, setViewType ] = useState("day");
-
-    useEffect(() => {
-        async function fetchData() {
-            const body = JSON.stringify(props.moduleCodes);
-            const response = await handlePost("/get-timetable", body, "application/json");
-            const data = await response.json();
-            setTimetable(data);
-        }
-        fetchData();
-    },[]);
-
-    useEffect(() => console.log(timetable), [timetable]);
-
-    let view = <div></div>;
-    switch (viewType) {
-        case "day":
-            view = <DayTimetable/>;
-            break;
-        case "week":
-            // view = <WeekTimetable/>;
-            view = <div></div>;
-            break;
-        case "month":
-            view = <MonthTimetable/>;
-            break;
-        default:
-            break;
-    }
-
-    return (
-        <>
-        TimetableVisualiser
-
-        <TimetableContext.Provider value={timetableUseState}>
-            <DateContext.Provider value={dateUseState}>
-                {view}
-            </DateContext.Provider>
-        </TimetableContext.Provider>
-
-        <hr/>
-        {JSON.stringify(timetable)}
-        </>
-    );
-}
-*/
---
+++ src/Calendar/misc-components.jsx
@@ -0,0 +1,29 @@
+/**
+ * Provides a set of three radio buttons that allows the user to choose
+ * how the 
+ * @param {object} props
+ * @param {Function} props.onChange
+ * @returns {JSX.Element}
+ */
+export function ViewRadios(props) {
+    // TODO: allow user to specify default view. Perhaps save value in localStorage?
+    let { onChange } = props;
+    return (
+        <div>
+            <label>
+                Day
+                <input type='radio' name="view" value="DAY" onChange={onChange}/>
+            </label>
+
+            <label>
+                Week
+                <input type='radio' name="view" value="WEEK" onChange={onChange} defaultChecked/>
+            </label>
+
+            <label>
+                Month
+                <input type='radio' name="view" value="MONTH" onChange={onChange}/>
+            </label>
+        </div>
+    );
+}
