5b5671c62ffb2ad84b59c58b77f0a31ff87c2ba1
mustcat <hwbrent@icloud.com>
2022-12-20 20:36:59 +0000

    Add useEffect to set date to Monday in Week view

--- src/Calendar/Week/Week.jsx
+++ src/Calendar/Week/Week.jsx
@@ -1,7 +1,22 @@
 import React, { useState, useEffect, useContext} from 'react';
 import { DateContext, TimetableContext } from '../Calendar';
+import { Weekdays } from '../misc/functions';
 
 export default function Week(props) {
+    const [ date, setDate ] = useContext(DateContext);
+
+    // whenever `date` is updated, 'round it' to the last monday if not already on monday
+    useEffect(() => {
+        if (date.getDay() !== Weekdays.Monday) {
+            setDate(prev => {
+                const copy = new Date(prev);
+                const diff = copy.getDay() - Weekdays.Monday;
+                copy.setDate(copy.getDate() - diff);
+                return copy;
+            });
+        }
+    }, [date]);
+
     return (
         <div>
             Week
